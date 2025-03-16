853836c55eb4f6a3ad3daf823582f5f4f168cdca
nikitazherebtsov <nikita@crimsongold.ru>
2025-03-16 22:40:36 +0500

    Пример с двумя парсерами

--- ch.js
+++ ch.js
@@ -1,83 +1,134 @@
-(function jsonGrammarOnlyExample() {
-    // ----------------- Lexer -----------------
-    const createToken = chevrotain.createToken;
-    const Lexer = chevrotain.Lexer;
-    const CstParser = chevrotain.CstParser;
-  
-    const Hash = createToken({ name: 'Hash', pattern: /#/ });
-    const Plus = createToken({ name: 'Plus', pattern: /\+/ });
-    const Text = createToken({ name: 'Text', pattern: /[^#+:\n]+/ });
-    const Newline = createToken({
-      name: 'Newline',
-      pattern: /\n/,
-      line_breaks: true,
-    });
-    const Whitespace = createToken({
-      name: 'Whitespace',
-      pattern: /[ \t]+/,
-      group: chevrotain.Lexer.SKIPPED,
-    });
-    
-    
-    const allTokens = [Whitespace, Hash, Text, Newline, Plus];
-    
-    
-    const MD_Lexer = new Lexer(allTokens, {
-      // Less position info tracked, reduces verbosity of the playground output.
-      positionTracking: "onlyStart",
-    });
-    
-    
-  class MD_Parser extends CstParser {
+(function MDDesignParsing() {
+
+  const createToken = chevrotain.createToken;
+  const Lexer = chevrotain.Lexer;
+  const CstParser = chevrotain.CstParser;
+  const EmbeddedActionsParser = chevrotain.EmbeddedActionsParser;
+
+  const Hash = createToken({ name: 'Hash', pattern: /#/, label: '#' });
+  const Plus = createToken({ name: 'Plus', pattern: /\+/, label: '+' });
+
+  const Slash = createToken({ name: 'Slash', pattern: /\// });
+  const Text = createToken({ name: 'Text', pattern: /[^#+:\n]+/ });
+  const Newline = createToken({
+    name: 'Newline',
+    pattern: /\n/,
+    line_breaks: true,
+  });
+  const Whitespace = createToken({
+    name: 'Whitespace',
+    pattern: /[ \t]+/,
+    group: chevrotain.Lexer.SKIPPED,
+  });
+
+
+  const allTokens = [Whitespace, Hash, Newline, Plus, Slash, Text];
+
+  const lexer = new chevrotain.Lexer(allTokens);
+
+  class LineParser extends CstParser {
     constructor() {
       super(allTokens);
       const $ = this;
-  
-      // Правила грамматики
-      $.RULE('Document', () => {
+
+      $.RULE('InlineStatement', () => {
         $.MANY(() => {
-          $.SUBRULE($.Group);
+          $.SUBRULE($.Input);
         });
       });
-  
-      $.RULE('Group', () => {
-        $.SUBRULE($.Header);
-        $.SUBRULE($.GroupLines);
-      });
-  
-      $.RULE('Header', () => {
-        $.AT_LEAST_ONE(() => {
-          $.CONSUME(Hash);
+
+      $.RULE('Input', () => {
+        $.MANY(() => {
           $.CONSUME(Text);
         });
-        $.CONSUME(Newline);
       });
-  
-      $.RULE('GroupLines', () => {
+
+      this.performSelfAnalysis();
+    }
+  };
+
+
+  const lineParser = new LineParser();
+
+
+  class GroupParser extends EmbeddedActionsParser {
+    constructor() {
+      super(allTokens);
+      const $ = this;
+
+      $.RULE('Form', () => {
+        let result = [];
+        $.MANY(() => {
+          result.push($.SUBRULE($.HGroup));
+        });
+        return result;
+      });
+
+      $.RULE('HGroup', () => {
+        let result = [];
+        let groups = [];
+
         $.AT_LEAST_ONE(() => {
-          $.OR([
-            { ALT: () => $.SUBRULE($.Header)},
-            { ALT: () => $.SUBRULE($.GroupLine)}
-          ]);
+
+          let header = $.SUBRULE($.VGroup);
+
+          result.push(header);
+          //header.children.VGroupLineItem = [];
+          //groups.push(header);
+        });
+
+        $.MANY(() => {
+          result.push($.SUBRULE($.VGroupLine, { ARGS: [groups] }));
           $.CONSUME(Newline);
         });
+        
+        lineParser.input = result;
+        return lineParser.InlineStatement();
       });
-  
-      $.RULE('GroupLine', () => {
+
+      $.RULE('VGroup', (groups) => {
+        return $.SUBRULE($.VGroupHeaderItem, groups);
+      });
+
+      $.RULE('VGroupHeaderItem', () => {
+        $.CONSUME(Hash);
+        return $.CONSUME(Text);
+      });
+
+      $.RULE('VGroupLine', (groups) => {
+        let result = [];
+        let index = 0;
         $.MANY(() => {
-          $.CONSUME(Plus);
-          $.CONSUME(Text);
+          let newItem = $.SUBRULE($.VGroupLineItem);
+          result.push(newItem);
+          //          if (groups !== undefined) {
+          //            groups[index].children.VGroupLineItem.push(newItem);
+          //            index++;
+          //};
         });
+        return result;
       });
-  
+
+
+      $.RULE('VGroupLineItem', () => {
+        $.CONSUME(Plus);
+        return $.CONSUME(Text);
+      });
+
+
+      $.RULE('Pages', () => { });
+      $.RULE('Page', () => { });
+
+      $.RULE('OneLineGroup', () => { });
+
       this.performSelfAnalysis();
     }
   }
-  
-    // for the playground to work the returned object must contain these fields
-    return {
-      lexer: MD_Lexer,
-      parser: MD_Parser,
-      defaultRule: 'Document'
-    };
-  }())
+
+  // for the playground to work the returned object must contain these fields
+  return {
+    lexer: lexer,
+    parser: GroupParser,
+    defaultRule: 'Form'
+  };
+}())
